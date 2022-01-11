7102f86e9adb806df58fb9b57f1e8d70b8442c2c
Nichole McGrew <nicholemcgrew@gmail.com>
2022-01-11 14:12:33 -0600

    Commit Operators Exercise

--- app.js
+++ app.js
@@ -7,13 +7,61 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let divide = a / b;
+let mod = a % b;
+let exponent = a ** b;
+
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
+console.log(mod);
+console.log(exponent);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(c + str);
+console.log(c + str2);
+console.log(c + isPresent);
+console.log(c + firstName);
+console.log(c + lastName);
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+
+console.log(d == str3); // true
+console.log(d === str3); // false
+console.log(!isPresent2); // true
+console.log("eleven" == str4 && d >= str3); //false
+console.log(!isPresent2 || isPresent2); //true
+console.log(0 == false); // true
+console.log(0 === false); // false
+console.log(0 != false); // false
+console.log(0 !== false); // true
+
+
